ffcad8a41b8121735c5667be67d9f38094fdc89e
Aleksandr Shevchuk <alekswebnet@gmail.com>
2024-11-28 12:41:37 +0200

    Clean deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 import path from 'path'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    dts(),
-    nodePolyfills()
+    dts()
   ],
   build: {
     lib: {
